464af42b9db25b0093dbfb37132126be44613606
raaksss <rakshitamanocha1@gmail.com>
2024-06-06 18:21:07 +0530

    Responsive Navbar added

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import MobileNav from './MobileNav'
+import MainNav from './MainNav'
 
 const Header = () => {
   return (
@@ -8,7 +10,12 @@ const Header = () => {
             <Link to="/" className="text-3xl font-bold tracking-tight text-orange-500">
                 WeEat.com
             </Link>
-
+            <div className='md:hidden'>
+                <MobileNav />
+            </div>
+            <div className='hidden md:block '>
+                <MainNav />
+            </div>
         </div>
 
     </div>
