6bf3f7dba463c23faadb712d9f5695cdd48cc5f1
raaksss <rakshitamanocha1@gmail.com>
2024-06-06 17:58:47 +0530

    Frontend and Backend base

---
+++ frontend/src/AppRoutes.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Layout from './layouts/Layout'
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+        <Route path="/" element={<Layout>Home </Layout>} />
+        <Route path="/user-profile" element={<span>User Profile Page</span>} />
+        <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  )
+}
+
+export default AppRoutes
