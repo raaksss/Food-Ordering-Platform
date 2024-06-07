6dd7144404ff121cbf739d754bd7c7caa52636ac
raaksss <rakshitamanocha1@gmail.com>
2024-06-07 14:57:48 +0530

    Create User frontend backend connected

--- frontend/src/auth/Auth0ProviderWithNavigator.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigator.tsx
@@ -1,3 +1,4 @@
+import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 import React from "react";
 
@@ -6,6 +7,7 @@ type Props = {
 };
 
 function Auth0ProviderWithNavigator({ children }: Props) {
+    const {createUser}= useCreateMyUser();
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -16,7 +18,9 @@ function Auth0ProviderWithNavigator({ children }: Props) {
     }
 
     const onRedirectCallback = (appState?: AppState, user?: User) => {
-        console.log("User", user);
+        if(user?.sub && user?.email){
+            createUser({auth0Id: user.sub, email:user.email});
+        }
     };
 
     return (
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,13 +5,23 @@ import './global.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import AppRoutes from './AppRoutes'
 import Auth0ProviderWithNavigator from './auth/Auth0ProviderWithNavigator'
+import { QueryClient, QueryClientProvider } from 'react-query'
+const queryClient= new QueryClient({
+  defaultOptions:{
+    queries:{
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
    <Router>
+    <QueryClientProvider client={queryClient}>
     <Auth0ProviderWithNavigator>
       <AppRoutes />
     </Auth0ProviderWithNavigator>
+    </QueryClientProvider>
    </Router>
   </React.StrictMode>,
 )
